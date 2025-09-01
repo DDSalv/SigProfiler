cf02f1cfa7c9999705f95ae28306352659461bab
Salvatore Di Dio <didiosalvatore93@gmail.com>
2025-09-01 16:43:16 +0200

    Update SigProfiler_SBS_DBS_ID.sh

--- SigProfiler_SBS_DBS_ID.sh
+++ SigProfiler_SBS_DBS_ID.sh
@@ -3,8 +3,6 @@ assignment_output_path_SBS="$vcf_input_and_matrix_output_path/SBS/"
 assignment_output_path_ID="$vcf_input_and_matrix_output_path/ID/"
 assignment_output_path_DBS="$vcf_input_and_matrix_output_path/DBS/"
 
-singularity exec -B /path/to/bind /path/to/container.sif python3  << EOF
-
 from SigProfilerAssignment import Analyzer as Analyze
 
 Analyze.cosmic_fit(samples="$vcf_input_and_matrix_output_path",
@@ -33,4 +31,3 @@ Analyze.cosmic_fit(samples="$vcf_input_and_matrix_output_path",
                    make_plots= True,
                    exclude_signature_subgroups=["Artifact_signatures"],
                    genome_build="GRCh38")
-EOF
